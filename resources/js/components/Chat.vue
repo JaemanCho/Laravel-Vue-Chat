3420f0152d0221cf24a4f84e3e692abd9faabd16
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2019-12-02 16:13:40 +0900

    Display user list in ChatUserList component

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="flex h-full">
-        <div class="w-1/5 border-r-2 border-solid border-gray-600">User List</div>
+        <ChatUserList />
         <div class="w-4/5 flex flex-col">
             <div class="flex-1">Chat Area</div>
             <div class="flex-initial p-2">
-                <input class="border-2 border-solid border-gray-600 w-full p-3" type="text">
+                <input class="border-2 border-solid rounded border-gray-600 w-full p-3" type="text">
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import ChatUserList from './ChatUserList';
     export default {
+        components: {
+            ChatUserList
+        },
         mounted() {
-            console.log('Component mounted.')
+            console.log('Chat Component mounted.')
         }
     }
 </script>
---
+++ resources/js/components/ChatUserList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-1/5 border-r-2 border-solid border-gray-600">
+        <div v-for="user in users" :key="user.id">
+            {{ user.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                users: []
+            }
+        },
+        created() {
+            axios.get('/api/users').then(res => {
+                console.log(res);
+                this.users = res.data.users;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        mounted() {
+            console.log('ChatUserList Component mounted.')
+        }
+    }
+</script>
